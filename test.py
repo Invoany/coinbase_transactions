0d7af399e896457f4143ea82b2910669b7133822
Invoany <jorgenunes88@gmail.com>
2023-01-06 02:03:41 +0000

    Added the full Blockchain Info Dict

--- test.py
+++ test.py
@@ -1,14 +1,12 @@
 from bitcoinrpc.authproxy import AuthServiceProxy, JSONRPCException
 rpc_connection = AuthServiceProxy("http://%s:%s@127.0.0.1:8332"%("umbrel", "MUwukWorrYkL75vkfZ6NMmM_lxGFw7h1hGTPIlDbJl8="))
-num_blocks_hash = rpc_connection.getbestblockhash()
-num_blocks = rpc_connection.getblockcount()
 blockchain_info = rpc_connection.getblockchaininfo()
-print("Hash of the best block - " + num_blocks_hash)
-print("Last Block - " + str(num_blocks))
-print(blockchain_info)
-print("----------")
-print(blockchain_info.keys())
 
-for key, value in blockchain_info:
-    print(key)
-    print(value)
+exclude_keys=["softforks","automatic_pruning","prune_target_size","pruneheight","initialblockdownload"]
+for key, value in blockchain_info.items():
+    if key == "size_on_disk":
+        print(key.title() +" - " + str(round(value/1024/1024/1024,2)) + " Gb")
+    elif key in exclude_keys:
+        pass
+    else:
+        print(key.title() +" - " + str(value))
