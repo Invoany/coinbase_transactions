1f85a9c162bc85c9b23ed8a591e438bfa87c11d0
Invoany <jorgenunes88@gmail.com>
2023-01-06 00:32:58 +0000

    Added block hash and count

--- test.py
+++ test.py
@@ -1,4 +1,14 @@
 from bitcoinrpc.authproxy import AuthServiceProxy, JSONRPCException
 rpc_connection = AuthServiceProxy("http://%s:%s@127.0.0.1:8332"%("umbrel", "MUwukWorrYkL75vkfZ6NMmM_lxGFw7h1hGTPIlDbJl8="))
+num_blocks_hash = rpc_connection.getbestblockhash()
 num_blocks = rpc_connection.getblockcount()
-print(num_blocks)
+blockchain_info = rpc_connection.getblockchaininfo()
+print("Hash of the best block - " + num_blocks_hash)
+print("Last Block - " + str(num_blocks))
+print(blockchain_info)
+print("----------")
+print(blockchain_info.keys())
+
+for key, value in blockchain_info:
+    print(key)
+    print(value)
