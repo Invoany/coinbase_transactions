3211c6a33cd356d36eb0ba7c785252f7b5cf9840
Invoany <jorgenunes88@gmail.com>
2023-01-09 01:53:39 +0000

    please add getrawtransaction

--- test.py
+++ test.py
@@ -1,12 +1,22 @@
 from bitcoinrpc.authproxy import AuthServiceProxy, JSONRPCException
+from datetime import datetime
 rpc_connection = AuthServiceProxy("http://%s:%s@127.0.0.1:8332"%("umbrel", "MUwukWorrYkL75vkfZ6NMmM_lxGFw7h1hGTPIlDbJl8="))
 blockchain_info = rpc_connection.getblockchaininfo()
 
-exclude_keys=["softforks","automatic_pruning","prune_target_size","pruneheight","initialblockdownload"]
+exclude_keys=["softforks","automatic_pruning","prune_target_size","pruneheight","initialblockdownload", "pruned"]
 for key, value in blockchain_info.items():
     if key == "size_on_disk":
         print(key.title() +" - " + str(round(value/1024/1024/1024,2)) + " Gb")
+    elif key == "time":
+        print(key.title() +" - " + str(datetime.fromtimestamp( value )))
+    elif key == "mediantime":
+        print(key.title() +" - " + str(datetime.fromtimestamp( value )))
     elif key in exclude_keys:
         pass
     else:
         print(key.title() +" - " + str(value))
+
+print("----------------")
+rpc_connection = AuthServiceProxy("http://%s:%s@127.0.0.1:8332"%("umbrel", "MUwukWorrYkL75vkfZ6NMmM_lxGFw7h1hGTPIlDbJl8="))
+tx_coinbase= rpc_connection.gettransaction ("000000000019d6689c085ae165831e934ff763ae46a2a6c172b3f1b60a8ce26f", "True")
+print(tx_coinbase)
