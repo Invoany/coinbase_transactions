87d86869687eaa63e150d56e2555e35987bb9ec2
Invoany <jorgenunes88@gmail.com>
2023-01-30 01:12:04 +0000

    Correction fo different transactions

--- test.py
+++ test.py
@@ -4,40 +4,17 @@ from decrypt import address, hex_to_ascii
 import pandas as pd
 
 rpc_connection = AuthServiceProxy("http://%s:%s@127.0.0.1:8332"%("umbrel", "MUwukWorrYkL75vkfZ6NMmM_lxGFw7h1hGTPIlDbJl8="))
-"""blockchain_info = rpc_connection.getblockchaininfo()
-
-exclude_keys=["softforks","automatic_pruning","prune_target_size","pruneheight","initialblockdownload", "pruned"]
-for key, value in blockchain_info.items():
-    if key == "size_on_disk":
-        print(key.title() +" - " + str(round(value/1024/1024/1024,2)) + " Gb")
-    elif key == "time":
-        print(key.title() +" - " + str(datetime.fromtimestamp( value )))
-    elif key == "mediantime":
-        print(key.title() +" - " + str(datetime.fromtimestamp( value )))
-    elif key in exclude_keys:
-        pass
-    else:
-        print(key.title() +" - " + str(value))"""
-
-"""rpc_connection = AuthServiceProxy("http://%s:%s@127.0.0.1:8332"%("umbrel", "MUwukWorrYkL75vkfZ6NMmM_lxGFw7h1hGTPIlDbJl8="))
-tx_coinbase= rpc_connection.getrawtransaction ("c13e29f092b13cf77f63c26f6adacb149b123c590e4a391625c1fc527d7ed400", True)
-#tx_coinbase= rpc_connection.getrawtransaction ("10b54fd708ab2e5703979b4ba27ca0339882abc2062e77fbe51e625203a49642", True)
-#tx_coinbase= rpc_connection.getrawtransaction ("02f754075a7fae665fa4440b799c711e319a56357eda6e6bc84d40220b200361", False)
-print(tx_coinbase)"""
-
-#print("----------------")
 exclude_keys_tx=["confirmations","n","locktime","blocktime"]
+
 all_blocks = pd.DataFrame()
 i = 0
-for block_height in range(150000,300000):
+for block_height in range(152249,152300):
     i += 1
-    #print(i)
     block_hash= rpc_connection.getblockhash(block_height)
     block= rpc_connection.getblock(block_hash)
     first_tx = block['tx'][0]
     tx_coinbase= rpc_connection.getrawtransaction (first_tx, True)
     tx_dict = pd.Series([],dtype=pd.StringDtype())
-    #print(block_height)
     tx_dict['block_height'] = block_height
     for key, value in tx_coinbase.items():
         if key == "txid":
@@ -47,25 +24,26 @@ for block_height in range(150000,300000):
                 for vin_key, vin_value in vin_dict.items():
                     if vin_key == "coinbase":
                         tx_dict['coinbase_hex'] = vin_value
-                        #print(vin_value)
                         tx_dict['coinbase_decoded'] = hex_to_ascii(vin_value).replace('\r', ' ').replace('\n', ' ').replace('\t', '')
         elif key == "vout" :
-            for vou_dict in value:
-                if len(value) == 1:
-                    for vou_key, vou_value in vou_dict.items():
-                        if vou_key == "scriptPubKey":
-                            for pub_key, pub_value in vou_value.items():
-                                if pub_key == "type":
-                                    tx_dict['type'] = pub_value
-                                elif pub_key == "asm":
-                                    asm_list = pub_value.split()
-                                    tx_dict['address'] = address(asm_list[0], False)
-                        elif vou_key == "value":
-                            tx_dict[vou_key] = vou_value
-                        else:
-                            pass
-                else:
-                    pass
+            all_vout = pd.DataFrame()
+            for tx_cb_receiver in value:
+                tx_vout = pd.Series([],dtype=pd.StringDtype())
+                tx_vout['value'] = format(tx_cb_receiver['value'],'.8f')
+                sc_dict = tx_cb_receiver['scriptPubKey']
+                if sc_dict['type'] == 'pubkeyhash':
+                    tx_vout['address']=sc_dict['address']
+                elif sc_dict['type'] == 'pubkey':
+                    asm_list = sc_dict['asm'].split()
+                    tx_vout['address'] = address(asm_list[0], False)
+                    asm_list=[]
+                df_teste = tx_vout.to_frame()
+                df_teste_T= df_teste.transpose()
+                all_vout = pd.concat([all_vout, df_teste_T])
+            all_vout.reset_index(inplace=True, drop=True)
+            all_vout.rename(columns={0:"value",1:'address'},inplace=True)
+            tx_dict['value'] = all_vout['value'].astype(float).sum()
+            tx_dict['address'] = ', '.join(all_vout['address'])
         elif key in exclude_keys_tx:
             pass
         else:
@@ -75,7 +53,7 @@ for block_height in range(150000,300000):
     df_T= df.transpose()
     df_T.set_index("block_height", inplace = True)
     all_blocks = pd.concat([all_blocks, df_T])
-    if i == 10:
+    if i > 10:
         all_blocks["value"] = pd.to_numeric(all_blocks["value"])
         all_blocks.to_csv('All_Coinbase_Transactions_{}.csv'.format(str(datetime.today().strftime('%Y%m%d'))), escapechar='\\')
         i=0
